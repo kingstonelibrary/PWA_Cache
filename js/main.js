3708be596bf8722d8c82c68515dc5456f5c1b5d5
ukonn_admin <UKONN@Mac-mini.local>
2019-02-24 13:08:48 +0900

    TC

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ if ('serviceWorker' in navigator) {
  }
 
  const channel = new MessageChannel();
- var updateBtn = document.getElementsByClassName('updateBtn');
+ var updateBtn = document.getElementsByClassName('updateBtn')[0];
  updateBtn.addEventListener("click", function(){
   navigator.serviceWorker.controller.postMessage('update', [channel.port2]);
  }, false);
