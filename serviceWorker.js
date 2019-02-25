1dd19181235b02879eebe5eb3dd926131b49ed54
ukonn_admin <UKONN@Mac-mini.local>
2019-02-25 15:13:07 +0900

    TX

--- serviceWorker.js
+++ serviceWorker.js
@@ -59,7 +59,7 @@ self.addEventListener('activate', event => {
         })
       );
     })
-    // self.clients.claim();   // active 状態になったらすぐにコントロールさせたい
+    self.clients.claim();   // active 状態になったらすぐにコントロールさせたい
   );
 });
 
